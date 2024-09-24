a1fcf313f5d34e053841c7b627a1ab58ae2f7a87
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-09-24 10:02:28 +0600

    Working with createDonateHistory function

--- js/index.js
+++ js/index.js
@@ -25,17 +25,28 @@ donateForNoakhali.addEventListener("click", function () {
   if (parseFloat(accountBalance.innerText) < 0) {
     alert("Not enough money");
   }
-  createDonatedHistory(donatedMoney, "Flood at Noakhali, Bangladesh");
+  createDonatedHistory(donatedAmountValue, "Flood at Noakhali, Bangladesh");
 });
 
 function createDonatedHistory(amount, place) {
-  const container = `
-    <div class="border-2 border-gray-500 p-3">
-      <h2 class="text-xl ">${amount} Taka is donated for ${place}</h2>
-      <p>${new Date().toLocaleDateString()}</p>
-    </div>
-  `;
+  // const container = `
+  //   <div class="border-2 border-gray-500 p-3">
+  //     <h2 class="text-xl ">${amount} Taka is donated for ${place}</h2>
+  //     <p>${new Date().toLocaleDateString()}</p>
+  //   </div>
+  // `;
+
+  const div = document.createElement("div");
+  div.className = "border-2 border-gray-500 rounded-sm p-3";
+  const h2 = document.createElement("h2");
+  h2.className = "text-xl text-center";
+  h2.innerText = `${amount} is donated for ${place}`;
+  const p = document.createElement("p");
+  p.innerText = `${new Date().toLocaleString()}`;
+  div.appendChild(h2);
+  div.appendChild(p);
+
   const history = document.getElementById("history");
-  history.innerHTML = container;
+  history.appendChild(div);
   console.log(history);
 }
